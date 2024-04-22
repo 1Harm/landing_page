57125e05abf2bbfe63c6de4892e605e2500be0a7
1Harm <ghost9029@mail.ru>
2024-04-23 00:30:22 +0500

    change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: false,
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
   },
 };
 
